7ebe2bd5a6c45841d3a6944dc3b1fc0528ff62c7
Justin Lange <well1912@gmail.com>
2016-08-16 13:42:15 -0700

    Add tests for better coverage of search

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -6,7 +6,8 @@ import pytest
 
 
 SEARCH_TABLE = [
-    (5, 5)
+    (5, [5], 5),
+    (5, [5, 4, 3, 2, 1], 5)
 ]
 
 DISPLAY_TABLE = [
@@ -59,13 +60,14 @@ def test_size():
     assert lst.size() == 3
 
 
-@pytest.mark.parametrize('val, result', SEARCH_TABLE)
-def test_search(val, result):
+@pytest.mark.parametrize('looking, values, result', SEARCH_TABLE)
+def test_search(looking, values, result):
     """Test search method."""
     from linked_list import LinkedList
     lst = LinkedList()
-    lst.push(val)
-    assert val == lst.search(5).value
+    for val in values:
+        lst.push(val)
+    assert looking == lst.search(looking).value
 
 
 def test_search_not_found():
