d6603912ca72876ba93ce2e7039a34c592990b82
Justin Lange <well1912@gmail.com>
2016-08-16 13:23:28 -0700

    Add docstrings, font encoding line

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""Implement stack abstract data type."""
+
 import linked_list
 
 
 class Stack():
+    """Stack abstract data type."""
+
     def __init__(self, iterable=None):
+        """Initialize the stack.
+
+        Fills it with values from iterable if provided."""
         self.linked_list = linked_list.LinkedList()
         linked_list.LinkedList.__init__(self.linked_list, iterable)
 
     def push(self, val):
+        """Push value onto the stack."""
         self.linked_list.push(val)
 
     def pop(self):
+        """Pop value from stack."""
         return self.linked_list.pop()
--- src/test_stack.py
+++ src/test_stack.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""Test stack.py."""
+
 import pytest
+
 
 INIT_TABLE = [
     ([1, 2, 3], 3),
@@ -11,12 +14,14 @@ INIT_TABLE = [
 
 @pytest.mark.parametrize('values, result', INIT_TABLE)
 def test_init(values, result):
+    """Test initialization of stack."""
     from stack import Stack
     stack = Stack(values)
     assert stack.pop() == result
 
 
 def test_stack():
+    """Test stack operations."""
     from stack import Stack
     stack = Stack()
     stack.push(1)
@@ -28,6 +33,10 @@ def test_stack():
 
 
 def test_pop_error():
+    """Test pop.
+
+    Ensure that pop raises an IndexError when called upon an empty
+    stack."""
     from stack import Stack
     stack = Stack()
     with pytest.raises(IndexError):
