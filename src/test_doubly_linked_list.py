5b2ca77d706bafd46a96acedbfb3f98fb4fac895
Justin Lange <well1912@gmail.com>
2016-08-17 13:11:18 -0700

    Implement test for dll push

---
+++ src/test_doubly_linked_list.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Test doubly_linked_list.py."""
+
+def test_push():
+    """Test push method."""
+    from doubly_linked_list import DoublyLinkedList
+    lst = DoublyLinkedList()
+    lst.push(0)
+    assert lst.head.value == 0
+    assert lst.tail.value == 0
+    lst.push(1)
+    assert lst.head.value == 1
+    assert lst.tail.value == 0
