4a866fd2106528536fad8f2d58390420d0fd06b6
Justin Lange <well1912@gmail.com>
2016-08-25 12:48:17 -0700

    Implement del_node

--- src/graph.py
+++ src/graph.py
@@ -17,3 +17,10 @@ class Graph(object):
         self.add_node(a)
         self.add_node(b)
         self._edges.append((a, b))
+
+    def del_node(self, n):
+        try:
+            self._nodes.remove(n)
+            self._edges = filter(lambda p: n not in p, self._edges)
+        except ValueError:
+            raise ValueError('Node not in graph')
