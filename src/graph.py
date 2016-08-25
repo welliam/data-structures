cdf9e052952fcb8406289edf0d5e19a04ae3e814
Justin Lange <well1912@gmail.com>
2016-08-25 13:48:14 -0700

    Interally keep nodes and edges as set
    for efficient membership checking

--- src/graph.py
+++ src/graph.py
@@ -1,34 +1,34 @@
 class Graph(object):
     def __init__(self):
-        self._nodes = []
-        self._edges = []
+        self._nodes = set()
+        self._edges = set()
 
     def add_node(self, n):
         if n not in self._nodes:
-            self._nodes.append(n)
+            self._nodes.add(n)
 
     def nodes(self):
-        return self._nodes
+        return list(self._nodes)
 
     def edges(self):
-        return self._edges
+        return list(self._edges)
 
     def add_edge(self, a, b):
         self.add_node(a)
         self.add_node(b)
-        self._edges.append((a, b))
+        self._edges.add((a, b))
 
     def del_node(self, n):
         try:
             self._nodes.remove(n)
             self._edges = filter(lambda p: n not in p, self._edges)
-        except ValueError:
+        except KeyError:
             raise ValueError('Node not in graph')
 
     def del_edge(self, a, b):
         try:
             self._edges.remove((a, b))
-        except ValueError:
+        except KeyError:
             raise ValueError('Node not in graph')
 
     def has_node(self, n):
