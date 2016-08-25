7bfc277be066e8564ded7c71520687808bdee503
Justin Lange <well1912@gmail.com>
2016-08-25 12:31:30 -0700

    Add add_edges tests

--- src/test_graph.py
+++ src/test_graph.py
@@ -20,3 +20,35 @@ def test_add_node(emptygraph, labels):
     for label in labels:
         emptygraph.add_node(label)
     assert all(map(lambda n: n in emptygraph.nodes(), labels))
+
+
+def test_add_edges(emptygraph):
+    emptygraph.add_node('a')
+    emptygraph.add_node('b')
+    emptygraph.add_edge('a', 'b')
+    assert ('a', 'b') in emptygraph.edges()
+
+
+def test_add_edges_2(emptygraph):
+    emptygraph.add_node('a')
+    emptygraph.add_node('b')
+    emptygraph.add_node('c')
+    emptygraph.add_edge('a', 'b')
+    emptygraph.add_edge('a', 'c')
+    assert ('a', 'c') in emptygraph.edges()
+
+
+def test_add_edges_3(emptygraph):
+    emptygraph.add_node('a')
+    emptygraph.add_node('b')
+    emptygraph.add_node('c')
+    emptygraph.add_edge('a', 'b')
+    emptygraph.add_edge('a', 'c')
+    assert ('b', 'c') not in emptygraph.edges()
+
+
+def test_add_edges_directed(emptygraph):
+    emptygraph.add_node('a')
+    emptygraph.add_node('b')
+    emptygraph.add_edge('a', 'b')
+    assert ('b', 'a') not in emptygraph.edges()
