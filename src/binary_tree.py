21336b8406fcf9f39d94f5cd92d8935c7f6ac1ff
Justin Lange <well1912@gmail.com>
2016-09-12 13:19:55 -0700

    Implement basic contains and insert

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -4,7 +4,24 @@
 
 
 class Node(object):
+    """Node for binary tree."""
     def __init__(self, left, value, right):
+        """Initialize node with given values."""
         self.left = left
         self.value = value
         self.right = right
+
+
+class BinaryTree(object):
+    """Binary tree."""
+    def __init__(self):
+        """Initialize binary tree."""
+        self.root = None
+
+    def contains(self, val):
+        """Check whether val is contained in the tree."""
+        return self.root.value == val
+
+    def insert(self, val):
+        """Insert a new value into the tree."""
+        self.root = Node(None, val, None)
