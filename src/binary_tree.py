3b6b122a7818ce804a9b383d065f8ed879ca9629
Justin Lange <well1912@gmail.com>
2016-09-12 13:26:54 -0700

    Make Node constructor branch arguments optional

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -5,7 +5,7 @@
 
 class Node(object):
     """Node for binary tree."""
-    def __init__(self, left, value, right):
+    def __init__(self, value, left=None, right=None):
         """Initialize node with given values."""
         self.left = left
         self.value = value
@@ -14,14 +14,24 @@ class Node(object):
 
 class BinaryTree(object):
     """Binary tree."""
-    def __init__(self):
-        """Initialize binary tree."""
-        self.root = None
-
-    def contains(self, val):
-        """Check whether val is contained in the tree."""
-        return self.root.value == val
-
-    def insert(self, val):
-        """Insert a new value into the tree."""
-        self.root = Node(None, val, None)
+    pass
+#     def __init__(self):
+#         """Initialize binary tree."""
+#         self.root = None
+# 
+#     def contains(self, val):
+#         """Check whether val is contained in the tree."""
+#         return self.root.value == val
+# 
+#     def insert(self, val):
+#         """Insert a new value into the tree."""
+#         if self.root is None:
+#             self.root = Node(None, val, None)
+#         current = self.root
+#         while True:
+#             if val < current.value:
+#                 if current.left is None:
+#                     current.left = 
+#             else:
+#                 # right branch
+#                 pass
--- src/test_binary_tree.py
+++ src/test_binary_tree.py
@@ -3,6 +3,36 @@
 """Test binary_tree.py"""
 
 
+def test_node_value():
+    from binary_tree import Node
+    assert Node(3, None, None).value == 3
+
+
+def test_node_left():
+    from binary_tree import Node
+    assert Node(None, 3, None).left == 3
+
+
+def test_node_right():
+    from binary_tree import Node
+    assert Node(None, None, 3).right == 3
+
+
+def test_unary_node_value():
+    from binary_tree import Node
+    assert Node(3).value == 3
+
+
+def test_unary_node_left():
+    from binary_tree import Node
+    assert Node(3).left is None
+
+
+def test_unary_node_right():
+    from binary_tree import Node
+    assert Node(3).right is None
+
+
 def test_contains():
     from binary_tree import BinaryTree
     t = BinaryTree()
@@ -10,16 +40,9 @@ def test_contains():
     assert t.contains(1)
 
 
-def test_node_value():
-    from binary_tree import Node
-    assert Node(None, 3, None).value == 3
-
-
-def test_node_left():
-    from binary_tree import Node
-    assert Node(3, None, None).left == 3
-
-
-def test_node_right():
-    from binary_tree import Node
-    assert Node(None, None, 3).right == 3
+def test_contains_deep():    
+    from binary_tree import BinaryTree
+    t = BinaryTree()
+    t.insert(1)
+    t.insert(2)
+    assert t.contains(1)
