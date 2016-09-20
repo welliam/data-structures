b3d410b20df10ceb0a0039d831cb1cb411386189
David Smith <dbsmith.dbs83@gmail.com>
2016-09-20 16:49:13 -0700

    fixed list building

--- src/sorting.py
+++ src/sorting.py
@@ -9,13 +9,13 @@ import timeit
 
 def build_lists():
     """Build lists for demo."""
-    list1 = [x for x in range(1000, 0)]
-    list2 = [x for x in range(0, 1000)]
-    list3 = [random.randint(0, 1000) for x in range(0, 1000)]
-    list4 = [random.randint(0, 1000) for x in range(0, 1000)]
-    list5 = [random.randint(0, 1000) for x in range(0, 1000)]
-    list6 = [random.randint(0, 1000) for x in range(0, 1000)]
-    list7 = [random.randint(0, 1000) for x in range(0, 1000)]
+    list1 = [x for x in range(0, 10000)]
+    list2 = [x for x in range(10000, 0, -1)]
+    list3 = [random.randint(0, 10000) for x in range(0, 10000)]
+    list4 = [random.randint(0, 10000) for x in range(0, 10000)]
+    list5 = [random.randint(0, 10000) for x in range(0, 10000)]
+    list6 = [random.randint(0, 10000) for x in range(0, 10000)]
+    list7 = [random.randint(0, 10000) for x in range(0, 10000)]
     return [list1, list2, list3, list4, list5, list6, list7]
 
 
@@ -23,6 +23,8 @@ def insertion_sort(list_in):
     """Implementation of Jon Bentley's insertion sort algorithm.
 
     List_in is a list of numbers.
+    Reference: Bentley, Jon (2000). Programming Pearls.
+    ACM Press/Addison–Wesley.
     """
     for i in range(2, len(list_in)):
         v = list_in[i]
@@ -37,18 +39,18 @@ if __name__ == '__main__':
     if len(sys.argv) != 1:
         print('usage: python sorting.py')
         sys.exit(0)
-    print('Let\'s sort some lists (1000 elements, 1000 times each)...')
+    print('Let\'s sort some lists (10000 elements, 100 times each)...')
     x = build_lists()
-    t = timeit.timeit(lambda: insertion_sort(x[0]), number=1000)
+    t = timeit.timeit(lambda: insertion_sort(x[0]), number=100)
     print('Best case: ', t)
 
-    t = timeit.timeit(lambda: insertion_sort(x[1]), number=1000)
+    t = timeit.timeit(lambda: insertion_sort(x[1]), number=100)
     print('Worst case: ', t)
 
     print('5 random cases:')
 
     for i in range(2, len(x)):
-        t = timeit.timeit(lambda: insertion_sort(x[i]), number=1000)
+        t = timeit.timeit(lambda: insertion_sort(x[i]), number=100)
         print('case:', t)
 
     print('program terminated.')
