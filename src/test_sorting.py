8f1410120ecb49f62105cefe454d0d1085764152
David Smith <dbsmith.dbs83@gmail.com>
2016-09-20 17:04:45 -0700

    tests passing

--- src/test_sorting.py
+++ src/test_sorting.py
@@ -6,7 +6,7 @@ import pytest
 
 LIST_TABLE = [
     ([x for x in range(0, 10)], [x for x in range(0, 10)]),
-    ([x for x in range(10, 0, -1)], [x for x in range(0, 10)]),
+    ([x for x in range(10, 0, -1)], [x for x in range(1, 11)]),
     ([5, 1, 4, 2, 3, 7, 6, 9, 8, 0], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
 ]
 
@@ -14,4 +14,5 @@ LIST_TABLE = [
 @pytest.mark.parametrize('list_in, list_out', LIST_TABLE)
 def test_insertion_sort(list_in, list_out):
     from sorting import insertion_sort
+    print(list_in)
     assert list_out == insertion_sort(list_in)
