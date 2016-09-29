21a7e9e2cab4fbeee209f0d1d8e84c16553f0b81
Justin Lange <well1912@gmail.com>
2016-09-29 16:01:05 -0700

    Implement radix sorting strings

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -2,10 +2,44 @@ from math import log10
 from src.queue import Queue
 
 
+ASCII_RADIX = 128
+
+
+def str_to_int(s, padding):
+    """Convert a string to an number.
+
+    Represents each character as a digit of a base-128 number."""
+    res = 0
+    for c in s:
+        print('1')
+        res *= ASCII_RADIX  # ascii
+        res += ord(c)
+        padding -= 1
+    res *= ASCII_RADIX ** padding
+    return res
+
+
+def int_to_str(n):
+    """Convert a number to a string.
+
+    Represents each character as a digit of a base-128 number."""
+    while n and n % 128 == 0:
+        n //= 128
+    res = ''
+    while n:
+        res = chr(n % 128) + res
+        n //= 128
+    return res
+
+
 def radix_sort(values):
     """Implement radix sort with a radix of 10."""
     if not values:
         return values
+    sorting_string = isinstance(values[0], str)
+    if sorting_string:
+        max_length = max(map(len, values))
+        values = [str_to_int(value, max_length) for value in values]
     buckets = [Queue() for _ in range(10)]
     for i in range(int(log10(max(values)) + 1)):
         for value in values:
@@ -14,4 +48,6 @@ def radix_sort(values):
         for bucket in buckets:
             while bucket.size():
                 values.append(bucket.dequeue())
+    if sorting_string:
+        return [int_to_str(value) for value in values]
     return values
--- src/test_radix_sort.py
+++ src/test_radix_sort.py
@@ -8,7 +8,17 @@ RADIX_SORT_LISTS = [
     [2, 1],
     range(100),
     range(100, 0, -1),
+    [213, 123, 64, 666, 456, 345, 38, 67, 8, 679, 58795, 89, 8, 9],
+    'lkfsdjlk',
+    ['a', 'ab', 'ba', 'b'],
+    ['askdhjasd',
+     'asldkjasldkjasdlkjaslkdjasldb',
+     'lskdjasldkjasdljkasdlkjasldkjasdlkjasdlkjasldkjaslkdja',
+     'b']
 ]
+
+STRS = ['', 'asd', 'asdfhkjasdfhasdf']
+PADDINGS = [16, 123, 50]  # all of these must be >= than len(max(STRS))
 
 
 @pytest.mark.parametrize('lst', RADIX_SORT_LISTS)
@@ -16,3 +26,19 @@ def test_radix_sort(lst):
     """Test radix sort sorts input."""
     from radix_sort import radix_sort
     assert radix_sort(lst) == sorted(lst)
+
+
+@pytest.mark.parametrize('s', STRS)
+@pytest.mark.parametrize('pad', PADDINGS)
+def test_string_encoding_1(s, pad):
+    """Test radix sort sorts input."""
+    from radix_sort import str_to_int, int_to_str
+    assert s == int_to_str(str_to_int(s, pad))
+
+
+@pytest.mark.parametrize('s', STRS)
+@pytest.mark.parametrize('pad', PADDINGS)
+def test_string_encoding_2(s, pad):
+    """Test radix sort sorts input."""
+    from radix_sort import str_to_int, int_to_str
+    assert s == int_to_str(str_to_int(int_to_str(str_to_int(s, pad)), pad))
